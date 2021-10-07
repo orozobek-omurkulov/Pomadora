ee3bd9ab5ea1651a43bc3cc2ca2d799af74da3cd
Orozobek <oroz1111omurkulov@gmail.com>
2021-10-07 16:16:02 +0600

    Updates

---
+++ precache-manifest.1311dcd66b39ac493d611b86db938d49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec3feaacad89c39801db6c0a2a03dbb",
+    "url": "/Pomadora/index.html"
+  },
+  {
+    "revision": "e852229e12594c1210ab",
+    "url": "/Pomadora/static/css/main.2a405a72.chunk.css"
+  },
+  {
+    "revision": "490acdc5fd92b832f2e1",
+    "url": "/Pomadora/static/js/2.14f1c4f5.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/Pomadora/static/js/2.14f1c4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e852229e12594c1210ab",
+    "url": "/Pomadora/static/js/main.5a0f25ca.chunk.js"
+  },
+  {
+    "revision": "cb6469137f5f197afa7d",
+    "url": "/Pomadora/static/js/runtime-main.42246c1d.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomadora/static/media/bell.525320df.flac"
+  }
+]);
